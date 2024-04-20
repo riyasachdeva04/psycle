5ee1933669108e81be795d8473af16e518eb15f7
riyasachdeva04 <riiya2048@gmail.com>
2024-04-20 17:02:31 +0530

    life sucks

--- app/main_app.py
+++ app/main_app.py
@@ -23,26 +23,11 @@ def about():
 @app.route('/test', methods=['GET', 'POST'])
 def autism_form():
     form = AutismForm()
-
     if request.method == 'POST' and form.validate_on_submit():
-        print("Social_Responsiveness_Scale:", form.Social_Responsiveness_Scale.data)
-        print("Age_Years:", form.Age_Years.data)
-        print("Qchat_10_Score:", form.Qchat_10_Score.data)
-        print("Speech_Delay_Language_Disorder:", form.Speech_Delay_Language_Disorder.data)
-        print("Learning_Disorder:", form.Learning_Disorder.data)
-        print("Genetic_Disorders:", form.Genetic_Disorders.data)
-        print("Depression:", form.Depression.data)
-        print("Global_Developmental_Delay:", form.Global_Developmental_Delay.data)
-        print("Social_Behavioural_Issues:", form.Social_Behavioural_Issues.data)
-        print("Childhood_Autism_Rating_Scale:", form.Childhood_Autism_Rating_Scale.data)
-        print("Sex:", form.Sex.data)
-        print("Family_mem_with_ASD:", form.Family_mem_with_ASD.data)
-        print("A10_score:", form.A10_score.data)
-
         prediction = form.predict()
-
-        print(f'Model Prediction: {prediction}', 'info')
-        return redirect(url_for('autism_form')) 
+        dict = {1: 'Likely Autistic', 0: 'Unlikely to be Autistic'}
+        alert_message = f'Model Prediction: {dict[prediction]}'
+        return render_template('test.html', form=form, alert_message=alert_message)
     return render_template('test.html', form=form)
 
 @app.route('/grow')  
